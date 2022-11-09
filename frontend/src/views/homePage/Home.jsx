318822b5f258507daf8e40d7c5133815907d72dd
Yohannes <uelandrae@gmail.com>
2022-11-09 15:00:24 +0100

    Data.js management

--- frontend/src/views/homePage/Home.jsx
+++ frontend/src/views/homePage/Home.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-
-import bitcoin from "../../assets/bitcoin-1.png";
-import ethereum from "../../assets/ethereum-1.png";
-import realEstate from "../../assets/real-estate.jpg";
-import bonds from "../../assets/money-1.jpg";
+import { HomePageTitleData } from "../../data/Data";
 import { HomePageData } from "../../data/Data";
 
 import "./Home.scss";
@@ -13,19 +8,13 @@ const Home = () => {
   return (
     <main className="home-page-container">
       <section className="investment-options">
-        <h1 className="title-of-home-page">Would you like to be financially independent?</h1>
-        <p className="paragraphs-environmental-analysis-home-page">
-          If your answer is yes, instead of you working to make money, make your money work for you. Hence, if you want to make money and get richer,
-          invest your money in <span> cryptocurrencies</span>, <span>stocks</span>, <span> real estate</span>, <span>bonds</span>, and <span> other securities</span>. Perhaps you may need <span className="span-tech"> technical advice</span> on <span className="span"> where</span>,
-          <span className="span"> how </span> and <span className="span">when</span> to invest. LisaConsult is here for you to make your dream come true. Just click the <NavLink to="/courseRegistration"> Registration Form</NavLink> to register faster for the course and you will enjoy an amazing life for
-          the rest of your life.
-        </p>
-
+        <h1 className="title-of-home-page"> {HomePageTitleData.heading} </h1>
+        <p className="stock-market-home-page"> {HomePageTitleData.paragraph} </p>
         <figure className="stock-market-elements">
-          <img src={bitcoin} alt="Bitcoin" />
-          <img src={ethereum} alt="Ethereum" />
-          <img className="real-estate" src={realEstate} alt="Real Estate" />
-          <img className="real-estate" src={bonds} alt="Real Estate" />
+          <img src={HomePageTitleData.images.bitCoin} alt="Bitcoin" />
+          <img src={HomePageTitleData.images.etheruem} alt="Ethereum" />
+          <img className="real-estate" src={HomePageTitleData.images.realEsate} alt="Real Estate" />
+          <img className="real-estate" src={HomePageTitleData.images.money} alt="Real Estate" />
         </figure>
       </section>
 
